1d3f40706e9986a55807c1227ed8e4922991859d
Igor Massaro <igorzagomassaro@gmail.com>
2024-08-09 21:06:53 -0300

    add providers; add providers implementations

---
+++ src/providers/IFoodInfoProvider.ts
@@ -0,0 +1,11 @@
+import FoodFileInfo from "../entities/FoodFileInfo";
+import Product from "../entities/Product";
+
+export interface IFoodFactsInfo {
+  products: Product[];
+  filesInfo: FoodFileInfo[]
+}
+
+export default interface IFoodInfoProvider {
+  getFoodFactsInfo(): Promise<IFoodFactsInfo>;
+}
---
+++ src/providers/implementations/OpenFoodFactsFoodInfoProvider.ts
@@ -0,0 +1,77 @@
+import FoodFileInfo from "../../entities/FoodFileInfo";
+import Product from "../../entities/Product";
+import { convertToProduct, downloadFile, readFileLines, readJsonFileLines, unlinkFile, unzipFile } from "../../utils";
+import IFoodInfoProvider, { IFoodFactsInfo } from "../IFoodInfoProvider";
+
+const BASE_FILE_URL = 'https://challenges.coode.sh/food/data/json';
+const BASE_FILE_NAME = 'index.txt';
+const MAX_LINES = 100;
+
+export default class OpenFoodFactsFoodInfoProvider implements IFoodInfoProvider {
+
+  constructor(
+    private files: FoodFileInfo[],
+  ) {}
+
+  private async getFileInfo(filePath: string): Promise<Product[]> {
+      const url = `${BASE_FILE_URL}/${filePath}`;
+      await downloadFile(url, filePath);
+      
+      const fileName = filePath.split('.').slice(0, 2).join('.');
+      await unzipFile(filePath, fileName);
+      unlinkFile(filePath);
+
+      const fileInfoIdx = this.findFileInfoIdx(fileName);
+      const lines = await readJsonFileLines(fileName, 
+        { 
+          startLine: fileInfoIdx >= 0 ? this.files[fileInfoIdx].startLine : 0, 
+          maxLines: MAX_LINES 
+        }
+      );
+      
+      unlinkFile(fileName);
+
+      this.updateFileInfoStartLine(fileInfoIdx, fileName);
+
+      return lines.map(line => convertToProduct(line))
+  }
+
+  private updateFileInfoStartLine(idx: number, fileName: string) {
+    if (idx < 0) {
+      this.files.push(new FoodFileInfo({
+        lastUpdate: '',
+        name: fileName,
+        startLine: 0
+      }));
+
+      idx = this.files.length - 1;
+    }
+
+    this.files[idx].startLine += MAX_LINES;
+    this.files[idx].lastUpdate = (new Date()).toISOString();
+  }
+
+  private findFileInfoIdx(fileName: string) {
+    const fileInfoIdx = this.files.findIndex(fInfo => fInfo.name === fileName);
+
+    return fileInfoIdx;
+  }
+
+  async getFoodFactsInfo(): Promise<IFoodFactsInfo> {
+    await downloadFile(`${BASE_FILE_URL}/${BASE_FILE_NAME}`, BASE_FILE_NAME);
+    const fileLines = await readFileLines(BASE_FILE_NAME, { limit: false });
+    unlinkFile(BASE_FILE_NAME);
+
+    const products: Product[] = [];
+    let count = 0;
+    for await (const filename of fileLines) {
+      const p = await this.getFileInfo(filename);
+      products.push(...p);
+
+      if (count == 2) { break; }
+      count++;
+    }
+
+    return { products, filesInfo: this.files };
+  }
+}
